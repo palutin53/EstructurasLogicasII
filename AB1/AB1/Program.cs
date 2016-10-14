7626e4c6d0858c558a5551b8def87f331e860eef
palutin53 <palutin31@gmail.com>
2016-10-14 11:11:28 -0600

    AB

---
+++ AB1/AB1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AB1
+{
+    class Program
+    {
+         static public void Main(String []args)
+        {
+
+        }
+        class Node
+        {
+            public int value;
+            public Node left;
+            public Node right;
+
+            public Node(int initial)        // Constructor
+            {
+                value = initial;
+                left = null;
+                left = null;
+            }
+        }
+        class Tree
+        {
+            Node top;
+            public Tree()
+            {
+                top = null;
+            }
+            public Tree(int initial)
+            {
+                top = new Node(initial);
+            }
+            public void Add(int value)
+            {
+                // non-recursive add
+                if (top == null)    // the tree is empty
+                {
+                    // add item has a base node
+                    Node NewNode = new Node(value);
+                    top = NewNode;
+                    return;
+                }
+                Node CurrentNode = top;
+                bool added = false;
+                do
+                {
+                    // Traverse tree
+                    if (value < CurrentNode.value)
+                    {
+                        // go left
+                        if (CurrentNode.left == null)
+                        {
+                            // add the item
+                            Node NewNode = new Node(value);
+                            CurrentNode.left = NewNode;
+                            added = true;
+                        }
+                        else
+                        {
+                            CurrentNode = CurrentNode.left;
+                        }
+                    }
+                    if(value>=CurrentNode.value)
+                    {
+                        if(CurrentNode.right==null)
+                        {
+                            Node NewNode = new Node(value);
+                            CurrentNode.right = NewNode;
+                            added = true;
+                        }
+                        else
+                        {
+                            // go right
+                            CurrentNode = CurrentNode.right;
+                        }
+                    }
+                } while (!added);
+            }
+            public void AddRc(int value)
+            {
+                // recursive add
+                AddR(ref top, value);
+            }
+            private void AddR(ref Node N, int value)
+            {
+                // private recursive search for where to add the new node
+                if (N == null)
+                {
+                    // Node doesn`t exist, so add it here
+                    Node NewNode = new Node(value);
+                    N = NewNode; // here, set the old node reference to the newly created node thus attaching it to the tree
+                    return; //end the function call a fall back
+                }
+                if (value < N.value)
+                {
+                    AddR(ref N.left, value);
+                    return;
+                }
+                if(value >= N.value)
+                {
+                    AddR(ref N.right, value);
+                    return;
+                }
+            }
+            public void Print(ref string newstring)
+            {
+                // write oout the tree in sorted order to the string newstring
+                // implement using recursion
+            }
+        }
+    }
+}
